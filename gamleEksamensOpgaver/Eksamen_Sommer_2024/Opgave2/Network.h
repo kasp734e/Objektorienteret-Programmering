d1d147b0c1d7f9c31f289696ecbe737921798e6d
Kasper P. B. Jensen <kasp734e@gmail.com>
2025-05-29 23:45:29 +0200

    Opgave 2 er også lavet i dag

--- gamleEksamensOpgaver/Eksamen_Sommer_2024/Opgave2/Network.h
+++ gamleEksamensOpgaver/Eksamen_Sommer_2024/Opgave2/Network.h
@@ -1,3 +1,70 @@
 #pragma once
-
+#include <vector>
+#include <stdexcept>
 //Opgave 2 a)
+template<typename T>
+class Network
+{
+public:
+	void addNode(const T& param1) {
+    nodes_.push_back(param1);
+  }
+
+	void addConnection(const T& first, const T& second) {
+    const T* first_p = findNode(first);
+    const T* second_p = findNode(second);
+
+    if (first_p == nullptr || second_p == nullptr)
+    {
+      throw std::invalid_argument("Node does not exist");
+    }
+
+    if (first == second)
+    {
+      throw std::invalid_argument("Self connection not allowed");
+    }
+    for (const auto& connection : connections_)
+    {
+      if (connection.first == first_p && connection.second == second_p || connection.first == second_p && connection.second
+        == first_p)
+      {
+        throw std::invalid_argument("Connection is already established");
+      }
+    }
+    connections_.emplace_back(first_p, second_p);
+}
+
+	std::vector<T> getAllNeighbours(const T& param1) const {
+    const auto node_p = findNode(param1);
+
+    std::vector<T> neighbours;
+
+    for (const auto& connection : connections_)
+    {
+      if (node_p == connection.first)
+      {
+        neighbours.push_back(*connection.second);
+      }
+      if (node_p == connection.second)
+      {
+        neighbours.push_back(*connection.first);
+      }
+    }
+	return neighbours;
+}
+	
+
+private:
+	const T* findNode(const T& to_find) const {
+	for (const auto& location : nodes_)
+	{
+		if (location == to_find)
+		{
+			return &location;
+		}
+	}
+	return nullptr;
+}
+	std::vector<T> nodes_;
+	std::vector<std::pair<const T*, const T*>> connections_;
+};
--- gamleEksamensOpgaver/Eksamen_Sommer_2024/Opgave2/TestNetwork.h
+++ gamleEksamensOpgaver/Eksamen_Sommer_2024/Opgave2/TestNetwork.h
@@ -1,2 +1,4 @@
 #pragma once
+#include "Location.h"
+#include "Network.h"
 void test_network();
